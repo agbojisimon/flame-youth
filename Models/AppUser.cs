d68c5a18803c857b2f55349fc1118fbb8ce366e4
Agboji Simon <agbojisimon107@gmail.com>
2025-12-27 22:57:24 +0100

    Extends AppUser Model

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace g_flame_youth.Models
 {
     public class AppUser : IdentityUser
     {
-
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}".Trim();
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
     }
 }
