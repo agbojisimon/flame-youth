296fa632591ac8afb3989f6c5e873f465a1c8257
Agboji Simon <agbojisimon107@gmail.com>
2025-12-31 13:54:49 +0100

    AnnouncementModel

---
+++ Models/Announcement.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace g_flame_youth.Models
+{
+    public class Announcement
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string CreatedById { get; set; } = string.Empty;
+        public AppUser CreatedBy { get; set; } = null!;
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public string Status { get; set; } = "Draft";
+        public string Category { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeletedOn { get; set; }
+    }
+}
