fd41226ecc36d68f97b490909674856bf2bbc962
Agboji Simon <agbojisimon107@gmail.com>
2025-12-31 13:57:42 +0100

    Modified UpdateDto

--- DTOs/User/UpdateUserDto.cs
+++ DTOs/User/UpdateUserDto.cs
@@ -11,19 +11,19 @@ namespace g_flame_youth.DTOs.User
         [Required]
         [MinLength(3, ErrorMessage = "Firstname cannot be less than 3 characters")]
         [MaxLength(50, ErrorMessage = "Firstname must not be greater than 50 characters")]
-        public string FirstName { get; set; } = string.Empty;
+        public string? FirstName { get; set; } = string.Empty;
         [Required]
         [MinLength(3, ErrorMessage = "Lastname cannot be less than 3 characters")]
         [MaxLength(50, ErrorMessage = "Lastname must not be greater than 50 characters")]
-        public string LastName { get; set; } = string.Empty;
+        public string? LastName { get; set; } = string.Empty;
         [Required]
         [MinLength(3, ErrorMessage = "Username cannot be less than 3 characters")]
         [MaxLength(50, ErrorMessage = "Username must not be greater than 50 characters")]
-        public string UserName { get; set; } = string.Empty;
+        public string? UserName { get; set; } = string.Empty;
         [Required]
         [EmailAddress]
-        public string Email { get; set; } = string.Empty;
+        public string? Email { get; set; } = string.Empty;
         [Required]
-        public string Password { get; set; } = string.Empty;
+        public string? Password { get; set; } = string.Empty;
     }
 }
