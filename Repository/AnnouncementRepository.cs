00e308e942469d249cf413fb4f2b3c77f872f379
Agboji Simon <agbojisimon107@gmail.com>
2026-01-01 14:50:02 +0100

    Modified Repository

--- Repository/AnnouncementRepository.cs
+++ Repository/AnnouncementRepository.cs
@@ -1,4 +1,5 @@
 using g_flame_youth.Data;
+using g_flame_youth.DTOs.Announcement;
 using g_flame_youth.Interfaces;
 using g_flame_youth.Models;
 using Microsoft.EntityFrameworkCore;
@@ -12,16 +13,64 @@ namespace g_flame_youth.Repository
         {
             _context = context;
         }
+        public async Task<Announcement> CreateAnnouncementAsync(Announcement announcement)
+        {
+            await _context.Announcements.AddAsync(announcement);
+            await _context.SaveChangesAsync();
+            return announcement;
+        }
+
+        public async Task<bool> DeleteAnnouncementAsync(int Id)
+        {
+            var announcement = await _context.Announcements.FirstOrDefaultAsync(a => a.Id == Id && !a.IsDeleted);
+
+            if (announcement == null)
+                return false;
+
+            announcement.IsDeleted = true;
+            announcement.DeletedOn = DateTime.UtcNow;
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
 
         public async Task<Announcement?> GetAnnouncementByIdAsync(int Id)
         {
-            return await _context.Announcements.FirstOrDefaultAsync(a => a.Id == Id && !a.IsDeleted);
+            return await _context.Announcements.Include(a => a.CreatedBy).FirstOrDefaultAsync(a => a.Id == Id && !a.IsDeleted);
         }
 
         public async Task<List<Announcement>> GetAnnouncementsAsync()
         {
-            return await _context.Announcements.Where(a => a.IsDeleted && a.Status == "Published").OrderByDescending(a => a.CreatedOn).ToListAsync();
+            return await _context.Announcements.Include(a => a.CreatedBy).Where(a => !a.IsDeleted && a.Status == "Published").OrderByDescending(a => a.CreatedOn).ToListAsync();
+        }
 
+        public async Task<Announcement?> UpdateAnnouncementAsync(int Id, UpdateAnnouncementDto updateAnnouncementDto)
+        {
+            var existingAnnouncement = await _context.Announcements
+        .FirstOrDefaultAsync(a => a.Id == Id && !a.IsDeleted);
+
+            if (existingAnnouncement == null)
+                return null;
+
+            existingAnnouncement.Title = updateAnnouncementDto.Title;
+            existingAnnouncement.Content = updateAnnouncementDto.Content;
+            existingAnnouncement.Status = updateAnnouncementDto.Status;
+            existingAnnouncement.Category = updateAnnouncementDto.Category;
+            existingAnnouncement.UpdatedOn = DateTime.UtcNow;
+
+            await _context.SaveChangesAsync();
+
+            // Explicitly load the CreatedBy navigation property
+            await _context.Entry(existingAnnouncement)
+                          .Reference(a => a.CreatedBy)
+                          .LoadAsync();
+
+            return existingAnnouncement;
+        }
+
+        internal async Task GetAnnouncementById(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
