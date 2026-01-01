01fadf9eb1a64804202fe3eb563a4c09ca0a864d
Agboji Simon <agbojisimon107@gmail.com>
2026-01-01 14:54:36 +0100

    Implementing Announcement Controller Request

---
+++ Controllers/Admin/AnnouncementController.cs
@@ -0,0 +1,75 @@
+using System.Security.Claims;
+using g_flame_youth.DTOs.Announcement;
+using g_flame_youth.Interfaces;
+using g_flame_youth.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace g_flame_youth.Controllers.Admin
+{
+    [Route("g-flame-youth/[controller]")]
+    [ApiController]
+    public class AnnouncementController : ControllerBase
+    {
+        private readonly IAnnouncementRepository _announcementRepository;
+        public AnnouncementController(IAnnouncementRepository announcement)
+        {
+            _announcementRepository = announcement;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAnnouncements()
+        {
+            var announcements = await _announcementRepository.GetAnnouncementsAsync();
+
+            var announcementDtos = announcements.Select(a => a.ToAnnouncementDto()).ToList();
+
+            return Ok(announcementDtos);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetAnnouncementById([FromRoute] int Id)
+        {
+            var announcement = await _announcementRepository.GetAnnouncementByIdAsync(Id);
+            if (announcement == null)
+                return NotFound($"Announcement with ID {Id} not found.");
+
+            var announcementDto = announcement.ToAnnouncementDto();
+
+            return Ok(announcementDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAnnouncement([FromBody] CreateAnnouncementDto createAnnouncementDto)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized();
+
+            var announcement = createAnnouncementDto.ToAnnouncementFromCreateDto(userId);
+
+            var createdAnnouncement = await _announcementRepository.CreateAnnouncementAsync(announcement);
+
+            return CreatedAtAction(
+                nameof(GetAnnouncementById),
+                new { id = createdAnnouncement.Id },
+                createdAnnouncement.ToAnnouncementDto()
+            );
+        }
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> UpdateAnnouncement([FromRoute] int Id, [FromBody] UpdateAnnouncementDto updateAnnouncementDto)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized();
+
+            var updatedAnnouncement = await _announcementRepository.UpdateAnnouncementAsync(Id, updateAnnouncementDto);
+
+            if (updatedAnnouncement == null)
+                return NotFound($"Announcement with ID {Id} not found.");
+
+            return Ok(updatedAnnouncement.ToAnnouncementDto());
+        }
+    }
+}
