061aa23cc8150b0a7bc0f8c3beaf3e8fbf6cbfac
Agboji Simon <agbojisimon107@gmail.com>
2026-01-01 14:47:08 +0100

    Modified Announcement Interface

--- Interfaces/IAnnouncementRepository.cs
+++ Interfaces/IAnnouncementRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using g_flame_youth.DTOs.Announcement;
 using g_flame_youth.Models;
 
 namespace g_flame_youth.Interfaces
@@ -10,5 +11,8 @@ namespace g_flame_youth.Interfaces
     {
         Task<List<Announcement>> GetAnnouncementsAsync();
         Task<Announcement?> GetAnnouncementByIdAsync(int Id);
+        Task<Announcement> CreateAnnouncementAsync(Announcement announcement);
+        Task<Announcement?> UpdateAnnouncementAsync(int Id, UpdateAnnouncementDto updateAnnouncementDto);
+        Task<bool> DeleteAnnouncementAsync(int Id);
     }
 }
