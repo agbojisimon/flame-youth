f77c36e8506ff1dda4fa37e06608b396d2821463
Agboji Simon <agbojisimon107@gmail.com>
2026-01-02 22:49:16 +0100

    Modified Logics

--- Interfaces/IAnnouncementRepository.cs
+++ Interfaces/IAnnouncementRepository.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using g_flame_youth.DTOs.Announcement;
+using g_flame_youth.Helpers;
 using g_flame_youth.Models;
 
 namespace g_flame_youth.Interfaces
 {
     public interface IAnnouncementRepository
     {
-        Task<List<Announcement>> GetAnnouncementsAsync();
+        Task<List<Announcement>> GetAnnouncementsAsync(AnnouncementQueryObject query);
         Task<Announcement?> GetAnnouncementByIdAsync(int Id);
         Task<Announcement> CreateAnnouncementAsync(Announcement announcement);
         Task<Announcement?> UpdateAnnouncementAsync(int Id, UpdateAnnouncementDto updateAnnouncementDto);
--- Repository/AnnouncementRepository.cs
+++ Repository/AnnouncementRepository.cs
@@ -1,5 +1,6 @@
 using g_flame_youth.Data;
 using g_flame_youth.DTOs.Announcement;
+using g_flame_youth.Helpers;
 using g_flame_youth.Interfaces;
 using g_flame_youth.Models;
 using Microsoft.EntityFrameworkCore;
@@ -39,9 +40,37 @@ namespace g_flame_youth.Repository
             return await _context.Announcements.Include(a => a.CreatedBy).FirstOrDefaultAsync(a => a.Id == Id && !a.IsDeleted);
         }
 
-        public async Task<List<Announcement>> GetAnnouncementsAsync()
+        public async Task<List<Announcement>> GetAnnouncementsAsync(AnnouncementQueryObject query)
         {
-            return await _context.Announcements.Include(a => a.CreatedBy).Where(a => !a.IsDeleted && a.Status == "Published").OrderByDescending(a => a.CreatedOn).ToListAsync();
+            var announcements = _context.Announcements.Include(a => a.CreatedBy).AsQueryable();
+
+            announcements = announcements.Where(a => !a.IsDeleted && a.Status == "Published");
+
+            if (!string.IsNullOrWhiteSpace(query.Title))
+            {
+                announcements = announcements.Where(a => a.Title.Contains(query.Title));
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("CreatedOn", StringComparison.OrdinalIgnoreCase))
+                {
+                    announcements = query.IsDescending ? announcements.OrderByDescending(a => a.CreatedOn) : announcements.OrderBy(a => a.CreatedOn);
+                }
+
+                else if (query.SortBy.Equals("Title", StringComparison.OrdinalIgnoreCase))
+                {
+                    announcements = query.IsDescending ? announcements.OrderByDescending(a => a.Title) : announcements.OrderBy(a => a.Title);
+                }
+            }
+            else
+            {
+                announcements = announcements.OrderByDescending(a => a.CreatedOn);
+            }
+
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await announcements.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Announcement?> UpdateAnnouncementAsync(int Id, UpdateAnnouncementDto updateAnnouncementDto)
@@ -60,17 +89,9 @@ namespace g_flame_youth.Repository
 
             await _context.SaveChangesAsync();
 
-            // Explicitly load the CreatedBy navigation property
-            await _context.Entry(existingAnnouncement)
-                          .Reference(a => a.CreatedBy)
-                          .LoadAsync();
+            await _context.Entry(existingAnnouncement).Reference(a => a.CreatedBy).LoadAsync();
 
             return existingAnnouncement;
-        }
-
-        internal async Task GetAnnouncementById(int id)
-        {
-            throw new NotImplementedException();
         }
     }
 }
