349376c028a50ffba8fa844d29f912b462575946
Agboji Simon <agbojisimon107@gmail.com>
2026-01-03 21:39:54 +0100

    Modified Announcement Repository

--- Repository/AnnouncementRepository.cs
+++ Repository/AnnouncementRepository.cs
@@ -14,11 +14,10 @@ namespace g_flame_youth.Repository
         {
             _context = context;
         }
-        public async Task<Announcement> CreateAnnouncementAsync(Announcement announcement)
+        public async Task CreateAnnouncementAsync(Announcement announcement)
         {
             await _context.Announcements.AddAsync(announcement);
             await _context.SaveChangesAsync();
-            return announcement;
         }
 
         public async Task<bool> DeleteAnnouncementAsync(int Id)
@@ -42,56 +41,30 @@ namespace g_flame_youth.Repository
 
         public async Task<List<Announcement>> GetAnnouncementsAsync(AnnouncementQueryObject query)
         {
-            var announcements = _context.Announcements.Include(a => a.CreatedBy).AsQueryable();
-
-            announcements = announcements.Where(a => !a.IsDeleted && a.Status == "Published");
+            var announcements = _context.Announcements.Include(a => a.CreatedBy).Where(a => !a.IsDeleted).AsQueryable();
 
             if (!string.IsNullOrWhiteSpace(query.Title))
             {
                 announcements = announcements.Where(a => a.Title.Contains(query.Title));
             }
-
             if (!string.IsNullOrWhiteSpace(query.SortBy))
             {
-                if (query.SortBy.Equals("CreatedOn", StringComparison.OrdinalIgnoreCase))
-                {
-                    announcements = query.IsDescending ? announcements.OrderByDescending(a => a.CreatedOn) : announcements.OrderBy(a => a.CreatedOn);
-                }
-
-                else if (query.SortBy.Equals("Title", StringComparison.OrdinalIgnoreCase))
-                {
-                    announcements = query.IsDescending ? announcements.OrderByDescending(a => a.Title) : announcements.OrderBy(a => a.Title);
-                }
+                announcements = query.SortBy.Equals("CreatedOn", StringComparison.OrdinalIgnoreCase) ? (query.IsDescending ? announcements.OrderByDescending(a => a.CreatedOn) : announcements.OrderBy(a => a.CreatedOn)) : announcements;
             }
             else
             {
                 announcements = announcements.OrderByDescending(a => a.CreatedOn);
             }
 
-            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+            var skip = (query.PageNumber - 1) * query.PageSize;
 
-            return await announcements.Skip(skipNumber).Take(query.PageSize).ToListAsync();
+            return await announcements.Skip(skip).Take(query.PageSize).ToListAsync();
         }
 
-        public async Task<Announcement?> UpdateAnnouncementAsync(int Id, UpdateAnnouncementDto updateAnnouncementDto)
+        public async Task UpdateAnnouncementAsync(Announcement announcement)
         {
-            var existingAnnouncement = await _context.Announcements
-        .FirstOrDefaultAsync(a => a.Id == Id && !a.IsDeleted);
-
-            if (existingAnnouncement == null)
-                return null;
-
-            existingAnnouncement.Title = updateAnnouncementDto.Title;
-            existingAnnouncement.Content = updateAnnouncementDto.Content;
-            existingAnnouncement.Status = updateAnnouncementDto.Status;
-            existingAnnouncement.Category = updateAnnouncementDto.Category;
-            existingAnnouncement.UpdatedOn = DateTime.UtcNow;
-
+            _context.Announcements.Update(announcement);
             await _context.SaveChangesAsync();
-
-            await _context.Entry(existingAnnouncement).Reference(a => a.CreatedBy).LoadAsync();
-
-            return existingAnnouncement;
         }
     }
 }
