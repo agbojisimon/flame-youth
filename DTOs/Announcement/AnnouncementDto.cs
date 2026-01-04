b729295c3755ccf54b8c584c11a94825112e97d2
Agboji Simon <agbojisimon107@gmail.com>
2026-01-04 21:03:33 +0100

    Modified Announcement Response Dto

--- DTOs/Announcement/AnnouncementDto.cs
+++ DTOs/Announcement/AnnouncementDto.cs
@@ -10,8 +10,7 @@ namespace g_flame_youth.DTOs.Announcement
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
-        public string CreatedById { get; set; } = string.Empty;
-        public string CreatedByName { get; set; } = string.Empty;
+        public string CreatedById { get; set; }
         public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
         public string Status { get; set; } = string.Empty;
         public string Category { get; set; } = string.Empty;
