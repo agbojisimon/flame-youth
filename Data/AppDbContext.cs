e0f24907360a324418399bb86b30263177d589df
Agboji Simon <agbojisimon107@gmail.com>
2026-01-04 21:14:45 +0100

    Added Event Entity and Validations

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -12,6 +12,7 @@ namespace g_flame_youth.Data
         }
 
         public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<Event> Events { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -20,11 +21,31 @@ namespace g_flame_youth.Data
            .HasIndex(u => u.Email)
            .IsUnique();
 
-            builder.Entity<Announcement>()
-            .HasOne(a => a.CreatedBy)
-            .WithMany()
-            .HasForeignKey(a => a.CreatedById)
-            .OnDelete(DeleteBehavior.Restrict);
+            builder.Entity<Announcement>().Property(a => a.CreatedById).IsRequired();
+
+            builder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+
+            entity.Property(e => e.Title).IsRequired().HasMaxLength(200);
+
+            entity.Property(e => e.Description).HasMaxLength(2000);
+
+            entity.Property(e => e.StartDate).IsRequired();
+
+            entity.Property(e => e.EndDate).IsRequired();
+
+            entity.Property(e => e.Location).IsRequired().HasMaxLength(300);
+
+            entity.Property(e => e.ImageUrl).HasMaxLength(500);
+
+            entity.Property(e => e.IsCancelled).HasDefaultValue(false);
+
+            entity.Property(e => e.IsDeleted).HasDefaultValue(false);
+
+            entity.Property(e => e.CreatedOn).HasDefaultValueSql("GETUTCDATE()");
+        });
+
         }
     }
 }
