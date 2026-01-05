c4b89481f83cdecbd6765bc762ed633c8912fb80
Agboji Simon <agbojisimon107@gmail.com>
2026-01-05 22:53:57 +0100

    Rearrange member  folder structure

---
+++ Controllers/Members/AccountController.cs
@@ -0,0 +1,93 @@
+using g_flame_youth.DTOs.Account;
+using g_flame_youth.Interfaces;
+using g_flame_youth.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace g_flame_youth.Controllers
+{
+    [Route("g-flame-youth/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
+        private readonly SignInManager<AppUser> _signManger;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+            _signManger = signInManager;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var appUser = new AppUser()
+            {
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName,
+                UserName = registerDto.UserName,
+                Email = registerDto.Email,
+                CreatedOn = DateTime.UtcNow
+            };
+
+            var createdUser = await _userManager.CreateAsync(appUser, registerDto.Password);
+
+            if (!createdUser.Succeeded)
+                return BadRequest(createdUser.Errors);
+
+            var roleResult = await _userManager.AddToRoleAsync(appUser, "Member");
+
+            if (!roleResult.Succeeded)
+                return BadRequest(roleResult.Errors);
+
+            return Ok(
+                new NewUserDto
+                {
+                    FirstName = appUser.FirstName,
+                    LastName = appUser.LastName,
+                    UserName = appUser.UserName,
+                    Email = appUser.Email,
+                    Token = await _tokenService.CreateTokenAsync(appUser)
+                }
+            );
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null)
+                return Unauthorized("Email or password does not exit");
+
+            var result = await _signManger.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded)
+                return Unauthorized("Email or password does not exist");
+
+            return Ok(
+                new NewUserDto
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    UserName = user.UserName,
+                    Email = user.Email,
+
+                    Token = await _tokenService.CreateTokenAsync(user)
+                }
+            );
+        }
+    }
+}
