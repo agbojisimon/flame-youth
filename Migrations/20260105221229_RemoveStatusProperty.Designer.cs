da5a22613aaf5ad57f539876f7d975e5608e8422
Agboji Simon <agbojisimon107@gmail.com>
2026-01-06 09:41:28 +0100

    retructure announcement property

--- Migrations/20260105221229_RemoveStatusProperty.Designer.cs
+++ Migrations/20260105221229_RemoveStatusProperty.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using g_flame_youth.Data;
 
@@ -11,9 +12,11 @@ using g_flame_youth.Data;
 namespace g_flame_youth.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260105221229_RemoveStatusProperty")]
+    partial class RemoveStatusProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
