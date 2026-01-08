8f4c4c158975e99a84f8c11a970dcd59b0f2bdc0
Agboji Simon <agbojisimon107@gmail.com>
2026-01-08 10:00:20 +0100

    Add Devotional Entity for ef Migrations

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -13,6 +13,8 @@ namespace g_flame_youth.Data
 
         public DbSet<Announcement> Announcements { get; set; }
         public DbSet<Event> Events { get; set; }
+        public DbSet<PrayerRequest> PrayerRequests { get; set; }
+        public DbSet<Devotional> Devotionals { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -24,28 +26,46 @@ namespace g_flame_youth.Data
             builder.Entity<Announcement>().Property(a => a.CreatedById).IsRequired();
 
             builder.Entity<Event>(entity =>
-        {
-            entity.HasKey(e => e.Id);
+            {
+                entity.HasKey(e => e.Id);
 
-            entity.Property(e => e.Title).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.Title).IsRequired().HasMaxLength(200);
 
-            entity.Property(e => e.Description).HasMaxLength(2000);
+                entity.Property(e => e.Description).HasMaxLength(2000);
 
-            entity.Property(e => e.StartDate).IsRequired();
+                entity.Property(e => e.StartDate).IsRequired();
 
-            entity.Property(e => e.EndDate).IsRequired();
+                entity.Property(e => e.EndDate).IsRequired();
 
-            entity.Property(e => e.Location).IsRequired().HasMaxLength(300);
+                entity.Property(e => e.Location).IsRequired().HasMaxLength(300);
 
-            entity.Property(e => e.ImageUrl).HasMaxLength(500);
+                entity.Property(e => e.ImageUrl).HasMaxLength(500);
 
-            entity.Property(e => e.IsCancelled).HasDefaultValue(false);
+                entity.Property(e => e.IsCancelled).HasDefaultValue(false);
 
-            entity.Property(e => e.IsDeleted).HasDefaultValue(false);
+                entity.Property(e => e.IsDeleted).HasDefaultValue(false);
 
-            entity.Property(e => e.CreatedOn).HasDefaultValueSql("GETUTCDATE()");
-        });
+                entity.Property(e => e.CreatedOn).HasDefaultValueSql("GETUTCDATE()");
+            });
 
+            builder.Entity<PrayerRequest>(entity =>
+            {
+                entity.HasKey(e => e.id);
+                entity.Property(e => e.Content).IsRequired().HasMaxLength(1000);
+            });
+
+            builder.Entity<Devotional>(entity =>
+            {
+                entity.HasKey(d => d.Id);
+                entity.Property(d => d.Title).IsRequired().HasMaxLength(200);
+                entity.Property(d => d.Content).IsRequired().HasMaxLength(2000);
+                entity.Property(d => d.DevotionalDate).IsRequired();
+                entity.Property(d => d.IsPublished).HasDefaultValue(false);
+                entity.Property(d => d.IsDeleted).HasDefaultValue(false);
+                entity.HasIndex(d => d.DevotionalDate).IsUnique();
+                entity.HasQueryFilter(d => !d.IsDeleted);
+                entity.Property(d => d.CreatedAt).HasDefaultValueSql("GETUTCDATE()");
+            });
         }
     }
 }
