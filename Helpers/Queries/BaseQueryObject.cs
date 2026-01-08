769cd9416b8a0e0d8b65d0c6dcc36ec319d85c76
Agboji Simon <agbojisimon107@gmail.com>
2026-01-08 10:01:31 +0100

    Refactor Base Query Object

--- Helpers/Queries/BaseQueryObject.cs
+++ Helpers/Queries/BaseQueryObject.cs
@@ -4,7 +4,7 @@ namespace g_flame_youth.Helpers.Queries
     public class BaseQueryObject
     {
         public int PageNumber { get; set; } = 1;
-        public int PageSize { get; set; } = 20;
+        public int PageSize { get; set; } = 10;
         public bool IsDescending { get; set; } = false;
         public string? SortBy { get; set; } = null;
     }
