f33bdab9897871fa96fde248630b989fc797a1d9
Agboji Simon <agbojisimon107@gmail.com>
2026-01-09 22:53:14 +0100

    refactor account Dto for clean and reusable code

---
+++ DTOs/Account/UserInfoDto.cs
@@ -0,0 +1,10 @@
+
+namespace g_flame_youth.DTOs.Account
+{
+    public class UserInfoDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
