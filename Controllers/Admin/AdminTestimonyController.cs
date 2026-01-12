01577059ca37a1eabe7890e64fdc62fe0680414e
Agboji Simon <agbojisimon107@gmail.com>
2026-01-12 22:10:17 +0100

    modify testimony controller features

--- Controllers/Admin/AdminTestimonyController.cs
+++ Controllers/Admin/AdminTestimonyController.cs
@@ -17,29 +17,6 @@ namespace g_flame_youth.Controllers.Admin
             _testimonyService = testimonyService;
         }
 
-        [HttpPost("create-testimony")]
-        public async Task<IActionResult> CreateTestimony([FromBody] CreateTestimonyDto createDto, string userId)
-        {
-            var testimony = await _testimonyService.CreateTestimonyAsync(createDto, userId);
-
-            if (testimony == null)
-            {
-                return BadRequest(new ApiResponse<TestimonyResponseDto?>
-                {
-                    isSuccess = false,
-                    Message = "Failed to create testimony",
-                    Data = null
-                });
-            }
-
-            return Ok(new ApiResponse<TestimonyResponseDto>
-            {
-                isSuccess = true,
-                Message = "Testimony created successfully",
-                Data = testimony
-            });
-        }
-
         [HttpGet("get-all-testimonies")]
         public async Task<IActionResult> GetTestimonies([FromQuery] TestimonyQueryObject query)
         {
