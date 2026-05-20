892e8e0a3e7c4bcbb87a99cf8de8d31cbb7b58d4
agbojisimon <agbojisimon107@gmail.com>
2026-05-20 19:27:00 +0100

    Fix sermon cache key to include query params - fixes pageSize bug

--- Services/SermonService.cs
+++ Services/SermonService.cs
@@ -38,7 +38,7 @@ namespace GlobalFlameMinistry.API.Services
             // Force published only for public
             query.IsPublished = true;
 
-            string cacheKey = CACHE_KEY_PUBLISHED_SERMONS;
+            string cacheKey = $"{CACHE_KEY_PUBLISHED_SERMONS}_p{query.PageNumber}_s{query.PageSize}_{query.SortBy}_{query.IsDescending}";
 
             // Attempt to retrieve from cache
             if (_memoryCache.TryGetValue(cacheKey, out PagedResult<SermonResponseDto>? cachedResult))
