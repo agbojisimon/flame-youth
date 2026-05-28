4161a53a6a1190b92c94bd83cce37f357a7d88f1
agbojisimon <agbojisimon107@gmail.com>
2026-05-28 12:11:39 +0100

    chore: add migration for AddSermonTheme

---
+++ Migrations/20260528102440_AddSermonTheme.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GlobalFlameMinistry.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSermonTheme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Theme",
+                table: "Sermons",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Theme",
+                table: "Sermons");
+        }
+    }
+}
